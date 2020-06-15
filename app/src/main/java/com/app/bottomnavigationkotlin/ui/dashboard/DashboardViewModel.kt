79fe75791c394324d2e86697bb97bbbde102fc73
Wyatt Evans <syattevans@gmail.com>
2020-06-15 01:43:09 -0500

    Added log tag to dashboard view model.

--- app/src/main/java/com/app/bottomnavigationkotlin/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/app/bottomnavigationkotlin/ui/dashboard/DashboardViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DashboardViewModel : ViewModel() {
+    private val simpleTag = this::class.java.simpleName
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
