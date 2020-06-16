e6eb19bc4bbbab5aa9e008e9b06783f6265f2e30
Wyatt Evans <syattevans@gmail.com>
2020-06-16 17:27:17 -0500

    Created activities package.

--- app/src/main/java/com/app/bottomnavigationkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/app/bottomnavigationkotlin/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app.bottomnavigationkotlin
+package com.app.bottomnavigationkotlin.activities
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.app.bottomnavigationkotlin.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +19,10 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+            R.id.navigation_home,
+            R.id.navigation_dashboard,
+            R.id.navigation_notifications
+        ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
