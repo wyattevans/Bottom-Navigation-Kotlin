53ed8eae8b258c035cc5b93d6451f5df2ea0c596
Wyatt Evans <syattevans@gmail.com>
2020-06-18 04:28:35 -0500

    Added log to on create function in main activity.

--- app/src/main/java/com/app/bottomnavigationkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/app/bottomnavigationkotlin/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.app.bottomnavigationkotlin.activities
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(simpleTag, "Main Activity loaded.")
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
