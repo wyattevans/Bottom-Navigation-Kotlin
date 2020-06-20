d4fcb19ca9222b09406dbad32e697711912f930e
Wyatt Evans <syattevans@gmail.com>
2020-06-20 09:14:22 -0500

    Added log to on create options menu.

--- app/src/main/java/com/app/bottomnavigationkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/app/bottomnavigationkotlin/activities/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        Log.d(simpleTag, "Options Menu loaded.")
         return true
     }
 }
