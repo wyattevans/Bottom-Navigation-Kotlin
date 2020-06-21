ec143c22bb32b2a40b349c735c5837e2a294b5de
Wyatt Evans <syattevans@gmail.com>
2020-06-21 09:07:01 -0500

    Removed comments from example instrumented test.

--- app/src/androidTest/java/com/app/bottomnavigationkotlin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/app/bottomnavigationkotlin/ExampleInstrumentedTest.kt
@@ -12,7 +12,6 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.app.bottomnavigationkotlin", appContext.packageName)
     }
